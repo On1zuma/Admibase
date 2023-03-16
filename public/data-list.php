9164dbeb7f5c41923ab89d0d19b5f6fc211236be
Wassim <96482486+On1zuma@users.noreply.github.com>
2023-03-16 22:17:33 +0100

    feat: navbar improvement (#9)

--- public/data-list.php
+++ public/data-list.php
@@ -1,38 +1,12 @@
 <?php
 
-session_start();
+include('navbar.php');
+
 if (empty($_SESSION['id'])) {
     header('Location: /');
 }
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIBD - Data list</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    
-<nav class="navbar navbar-dark bg-primary">
-  <a class="navbar-brand" href="/">AdmiBase</a>
-
-  <ul class="navbar-nav mr-auto" style="display:flex; flex-direction:row; gap:1rem;">
-    <li class="nav-item active">
-        <a class="nav-link" href="/list-table.php">Table list</a>
-    </li>
-    <li class="nav-item active">
-        <a class="nav-link" href="#"> PhpMyAdmin</a>
-    </li>
-    <li class="nav-item active">
-        <a class="nav-link" href="/logout.php"> Logout</a>
-    </li>
-  </ul>
-</nav>
 
 <div class="mx-auto" style="width: 70vw; margin-top: 2rem;">
   <div class="filter" style="margin-bottom: 1rem; display:flex; align-items:center; flex-direction:row; justify-content:space-between;">
---
+++ public/navbar.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIBD - Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-primary">
+  <a class="navbar-brand" href="/">AdmiBase</a>
+
+  <ul class="navbar-nav mr-auto" style="display:flex; flex-direction:row; gap:1rem;">
+
+    <li class="nav-item active">
+        <a class="nav-link" href="#"> PhpMyAdmin</a>
+    </li>
+    <?php if (!isset($_SESSION['id'])): // check if user is not login so we show that link?>
+    <li class="nav-item active">
+        <a class="nav-link" href="/login.php"> Login</a>
+    </li>
+    <li class="nav-item active">
+        <a class="nav-link" href="/login.php"> Register</a>
+    </li>
+    <?php endif; ?>
+
+    <?php if (isset($_SESSION['id']) && !empty($_SESSION['id'])): // check if user is logged in so we only show that links?>
+    <li class="nav-item active">
+        <a class="nav-link" href="/list-table.php">Table list</a>
+    </li>
+    <li class="nav-item active">
+        <a class="nav-link" href="/logout.php"> Logout</a>
+    </li>
+    <?php endif; ?>
+
+  </ul>
+</nav>
