2753756e4fc76ab97b4ac70c5ed2ece27f317904
Florian GROSJEAN <floriangrosjean70@gmail.com>
2023-03-16 17:02:24 +0100

    feat: get acces right

--- Model/admin.php
+++ Model/admin.php
@@ -27,7 +27,7 @@ class Admin {
     }
 
     public function connectUser($username, $userpassword) {
-
+        $table = array();
         // Requête SQL pour sélectionner l'utilisateur et vérifier le mot de passe
         $sql = "SELECT User, Password FROM mysql.user WHERE User = '$username'";
 
@@ -45,8 +45,9 @@ class Admin {
                  $this->user = $username;
                  $this->password = $userpassword;
                  $this->__construct();
+                 $table = $this->rightUser($username);
                  echo "L'utilisateur $username est maintenant connecté";
-
+                 return $table;
             } else {
                 echo "L'utilisateur $username existe mais le mot de passe est incorrect !";
             }
@@ -54,5 +55,59 @@ class Admin {
             echo "L'utilisateur $username n'existe pas !";
         }
 
+
     }
+
+    public function rightUser($username) {
+        $tableAndRights = array();
+    
+        switch ($username) {
+            case 'admin_db':
+                $tableAndRights['table'] = '*';
+                $tableAndRights['access'] = ['ALL PRIVILEGES'];
+                break;
+            case 'admin_privileges':
+                $tableAndRights['table'] = '*';
+                $tableAndRights['access'] = ['GRANT OPTION'];
+                break;
+            case 'responsable_optimisation':
+                $tableAndRights['table'] = '*';
+                $tableAndRights['access'] = ['INDEX'];
+                break;
+            case 'tache_automatisee':
+                $tableAndRights['table'] = 'taches_auto';
+                $tableAndRights['access'] = ['RELOAD, SHUTDOWN, PROCESS'];
+                break;
+            case 'developpeur':
+                $tableAndRights['table'] = '*';
+                $tableAndRights['access'] = ['SELECT, INSERT, UPDATE, DELETE'];
+                break;
+            case 'moderateur_communaute':
+                $tableAndRights['table'] = 'joueur';
+                $tableAndRights['access'] = ['UPDATE, DELETE'];
+                break;
+            case 'specialiste_metier':
+                $tableAndRights['table'] = 'chevaux';
+                $tableAndRights['access'] = ['SELECT, UPDATE'];
+                break;
+            case 'admin_concours':
+                $tableAndRights['table'] = 'concours';
+                $tableAndRights['access'] = ['SELECT, INSERT, UPDATE, DELETE'];
+                break;
+            case 'editorialiste':
+                $tableAndRights['table'] = 'journal_quotidien';
+                $tableAndRights['access'] = ['SELECT, INSERT, UPDATE, DELETE'];
+                break;
+            case 'client':
+                $tableAndRights['table'] = 'concours, journal_quotidien';
+                $tableAndRights['access'] = ['SELECT'];
+                break;
+            default:
+                $tableAndRights['table'] = null;
+                $tableAndRights['access'] = null;
+        }
+        
+        return $tableAndRights;
+    }
+
 }
