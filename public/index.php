576d22920761acdfda824c8c3488ea617214b9db
On1zuma <wassi.mess@hotmail.com>
2023-03-16 21:25:49 +0100

    feat: login form is working

--- public/index.php
+++ public/index.php
@@ -1,4 +1,32 @@
-<?php ?>
+<?php
+session_start();
+if ($_SESSION['id']) {
+    header('Location: list-table.php');
+}
+
+$bdd = new PDO('mysql:host=localhost;dbname=gamedb;charset=utf8;', 'root', '');
+if (isset($_POST['send'])) {
+    if (!empty($_POST['login']) and !empty($_POST['password'])) {
+        $login = htmlspecialchars($_POST['login']);
+        $password = htmlspecialchars($_POST['password']);
+        // $password = sha1($_POST['password']);
+
+        $getUsers = $bdd->prepare('SELECT * FROM joueur WHERE pseudonyme = ? AND password = ?');
+        $getUsers->execute(array($login, $password));
+
+        if ($getUsers->rowCount() > 0) {
+            $_SESSION['id'] = $getUsers->fetch()['id'];
+            $_SESSION['pseudonyme'] = $login;
+            $_SESSION['password'] = $password;
+            header('Location: list-table.php');
+        } else {
+            echo "Please enter good username and password...";
+        }
+    } else {
+        echo "Please enter good username and password...";
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,17 +54,17 @@
 </nav>
 
 <div class="mx-auto" style="width: 70vw; margin-top: 2rem;">
-    <form style="margin-bottom: 2rem;" action="../Controller/connectionController.php" method="post">
+    <form style="margin-bottom: 2rem;" method="post"> <!-- action="../Controller/connectionController.php" -->
     <div class="form-group">
         <label for="username">Login</label>
-        <input type="text" class="form-control" id="username" aria-describedby="loginHelp" placeholder="Enter login">
+        <input type="text" class="form-control" id="username" aria-describedby="loginHelp" placeholder="Enter login" name="login">
         <small id="loginHelp" class="form-text text-muted">We'll never share your data with anyone else.</small>
     </div>
     <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" placeholder="Password">
+        <input type="password" class="form-control" id="password" placeholder="Password" name="password">
     </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" name="send">Submit</button>
     </form>
 
     <div class="passwords">
