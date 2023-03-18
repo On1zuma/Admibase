d2b3a0d64ded53ac9d25ef0a726ac0116dd70af8
Wassim <96482486+On1zuma@users.noreply.github.com>
2023-03-18 12:15:35 +0100

    feat: list of tables which depends on rights (#12)

--- public/list-table.php
+++ public/list-table.php
@@ -1,21 +1,32 @@
 <?php
-
 include('./components/navbar.php');
 
 if (empty($_SESSION['id'])) {
     header('Location: login.php');
 }
 
-$tables = ["Horse", "House", "Cars", "Race","other"];
+$table_name = $_SESSION['id']['table'];
+$bdd = new PDO('mysql:host=localhost;dbname=gamedb;charset=utf8;', 'root', '');
 
+if ($table_name != "*") {
+    $tables = explode(", ", $table_name);
+} else {
+    $getTables = $bdd->query('SHOW TABLES');
+    $tables = array();
+
+    while ($row = $getTables->fetch(PDO::FETCH_NUM)) {
+        $tableName = $row[0];
+        $tables[] = $tableName;
+    }
+}
 ?>
 
 <div class="mx-auto" style="width: 70vw; margin-top: 2rem;">
   <ul class="list-group">
-  <?php  foreach ($tables as $table) {
-      echo "<li class='list-group-item'><a href='list-data.php?table=".$table."'>".$table."</a></li>";
-  }
-?>
+  <?php foreach ($tables as $table) {
+      $tableWithSpaces = str_replace("_", " ", $table);
+      echo "<li class='list-group-item'><a href='list-data.php?table=".$table."'>".$tableWithSpaces."</a></li>";
+  } ?>
   </ul>
 </div>
 
