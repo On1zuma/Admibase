3d8e2135ba9c095c2cd2fd3c9b0085db7b31359a
On1zuma <wassi.mess@hotmail.com>
2023-03-18 15:21:05 +0100

    refactoring: checkIfUserCanAccessTable

--- public/list-data.php
+++ public/list-data.php
@@ -5,27 +5,11 @@ include('./components/navbar.php');
 $status = new RightController();
 $status->isLoggedIn();
 
-$bdd = new PDO('mysql:host=localhost;dbname=gamedb;charset=utf8;', 'root', '');
-
-$table_name = $_SESSION['id']['table']; // users tables
-$tableUrl = $_GET['table']; // table set in the url
-$stmt = $bdd->prepare("SHOW TABLES");
-$stmt->execute();
-$tablesBd = $stmt->fetchAll(PDO::FETCH_COLUMN);
+$data = new DataController();
+$tableUrl = $tables = $data->checkIfUserCanAccessTable();
 
 $tableUrlWithSpaces = str_replace('_', ' ', $tableUrl);
 
-// Check if the table exists in the database
-if (!in_array($tableUrl, $tablesBd)) {
-    header('Location: 404.php');
-    exit; // stop the script
-}
-
-// Check if the user has access to the table
-if ($table_name != "*" && !in_array($tableUrl, explode(", ", $table_name))) {
-    header('Location: list-table.php');
-    exit; // stop the script
-}
 ?>
 
 <div class="mx-auto" style="width: 70vw; margin-top: 2rem;">
