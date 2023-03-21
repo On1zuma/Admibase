e4e4adce253e7a1a36cff40b78c493b0afb991df
Florian GROSJEAN <floriangrosjean70@gmail.com>
2023-03-21 11:27:53 +0100

    qa: code clean

--- public/login.php
+++ public/login.php
@@ -10,32 +10,10 @@ if (isset($_POST['send']) && empty($_SESSION['id'])) {
     $objet->handleFormSubmission($_POST["login"], $_POST["password"]);
 }
 
-// $bdd = new PDO('mysql:host=localhost;dbname=gamedb;charset=utf8;', 'root', '');
-// if (isset($_POST['send']) && $_POST['send'] == 2) {
-//     if (!empty($_POST['login']) and !empty($_POST['password'])) {
-//         $login = htmlspecialchars($_POST['login']);
-//         $password = htmlspecialchars($_POST['password']);
-//         // $password = sha1($_POST['password']);
-
-//         $getUsers = $bdd->prepare('SELECT * FROM joueur WHERE pseudonyme = ? AND password = ?');
-//         $getUsers->execute(array($login, $password));
-
-//         if ($getUsers->rowCount() > 0) {
-//             $_SESSION['id'] = $getUsers->fetch()['id'];
-//             $_SESSION['pseudonyme'] = $login;
-//             $_SESSION['password'] = $password;
-//             header('Location: list-table.php');
-//         } else {
-//             echo "Please enter good username and password...";
-//         }
-//     } else {
-//         echo "Please enter good username and password...";
-//     }
-// }
 ?>
 
 <div class="mx-auto" style="width: 70vw; margin-top: 2rem;">
-    <form style="margin-bottom: 2rem;" method="post" action=""> <?php //../Controller/connectionController.php?>
+    <form style="margin-bottom: 2rem;" method="post" action=""> 
         <div class="form-group">
             <label for="username">Login</label>
             <input type="text" class="form-control" id="username" aria-describedby="loginHelp" placeholder="Enter login" name="login">
